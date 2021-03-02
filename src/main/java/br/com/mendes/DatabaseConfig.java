4702bd43bc2fa38ab357859181ea899c27f1c918
Rafael Ribeiro Bonamigo <32879433+bonamigor@users.noreply.github.com>
2021-03-02 15:36:39 -0300

    Update imports

--- src/main/java/br/com/mendes/DatabaseConfig.java
+++ src/main/java/br/com/mendes/DatabaseConfig.java
@@ -1,5 +1,8 @@
 package br.com.mendes;
 
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
