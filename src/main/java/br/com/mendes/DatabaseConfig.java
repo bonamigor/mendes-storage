a6c5c374bff283cfd9b6d75d9b1c693211e55161
Rafael Ribeiro Bonamigo <32879433+bonamigor@users.noreply.github.com>
2021-03-02 15:34:21 -0300

    Data config para heroku

---
+++ src/main/java/br/com/mendes/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package br.com.mendes;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Configuration
+@PropertySource({ "classpath:application.properties" })
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
