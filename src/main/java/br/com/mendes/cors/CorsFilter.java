9eb9adca9aa87062a3f03c7537d1f1d5d702c7f2
Rafael Ribeiro Bonamigo <32879433+bonamigor@users.noreply.github.com>
2021-03-03 16:28:11 -0300

    Update CorsFilter.java

--- src/main/java/br/com/mendes/cors/CorsFilter.java
+++ src/main/java/br/com/mendes/cors/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) resp;
 
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        response.setHeader("Access-Control-Allow-Headers: Content-Type");
+        response.setHeader("Accesss-Control-Allow-Headers", "Authorization, Content-Type, Accept");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
