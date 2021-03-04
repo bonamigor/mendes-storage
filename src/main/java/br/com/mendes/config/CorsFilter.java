d48bf2a33fe8ffdea102d60a5dba9a358e2bfa48
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2021-03-03 21:25:06 -0300

    Configurando o CORS para app front-end

--- src/main/java/br/com/mendes/config/CorsFilter.java
+++ src/main/java/br/com/mendes/config/CorsFilter.java
@@ -1,9 +1,12 @@
 package br.com.mendes.config;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class CorsFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
