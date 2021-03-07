f5313d6a49b5b859fab6c6e83559ec09f15789f1
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2021-03-07 15:28:51 -0300

    Ajuste do CORS pois app angular também já está no heroku

--- src/main/java/br/com/mendes/config/CorsFilter.java
+++ src/main/java/br/com/mendes/config/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "https://bonamigo.herokuapp.com/");
+        response.setHeader("Access-Control-Allow-Origin", "https://bonamigo.herokuapp.com");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
